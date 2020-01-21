6ec3fcfedd4c4bd8663300c613a7263cae1566c0
Christian Murillo <cm242@rutgers.edu>
2020-01-20 20:49:42 -0500

    initial work

---
+++ src/app/app.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./app.css";
+import Header from "../header/header";
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="app">
+        <Header />
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/header/header.js
@@ -0,0 +1,31 @@
+import React from "react";
+import "./header.css";
+
+class Header extends React.Component {
+  render() {
+    return (
+      <div className="header">
+        <h2>Dark Apps</h2>
+        <p className="social">
+          <a
+            href="https://github.com/christianmurillo/dark-apps"
+            rel="noopener noreferrer"
+            target="_blank"
+          >
+            <i className="fa fa-github fa-lg" aria-hidden="true" />
+          </a>
+          &nbsp;
+          <a
+            href="https://www.linkedin.com/in/christian-murillo-1353b098/"
+            rel="noopener noreferrer"
+            target="_blank"
+          >
+            <i className="fa fa-linkedin-square fa-lg" aria-hidden="true" />
+          </a>
+        </p>
+      </div>
+    );
+  }
+}
+
+export default Header;
